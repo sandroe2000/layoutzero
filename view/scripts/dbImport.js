756b4b5a719901366e3c2bd7fd3fad6ba2dad76a
Sandro C Espindula <sandroe2000@gmail.com>
2019-01-18 01:31:18 -0200

    Add DB Connection CRUD

--- view/scripts/dbImport.js
+++ view/scripts/dbImport.js
@@ -5,11 +5,6 @@ let dbImport = {
         document.querySelectorAll('input[name="chkField"]').forEach(chk => {
             chk.addEventListener('click', dbImport.chkField, false);
         });
-        document.querySelectorAll('select[name="fieldDestiny"]').forEach(select => {
-            select.addEventListener('change', dbImport.optionSelected, false);
-            dbImport.ArrCombos[i] = 0;
-            i++;
-        });
     },
     newConn: () => {
         let title = 'Cadastro - New DB Conection';
@@ -30,32 +25,6 @@ let dbImport = {
         row.lastElementChild.lastElementChild.disabled=chk.checked
         row.style.background=bg;
         row.firstChild.nextSibling.style.color=font;
-    },
-    ArrCombos: [],
-    optionSelected: (event) => {
-        /* 1 - Array com um item para cada select
-         * 2 - Cada item do Array inicia com o valor zerado representando o selectedIndex de cada Select.
-         *
-         * Quando selecionar um item, exemplo do select 1 item 3, 
-         * verifica se este item esta marcado no Array com valor diferente de zero.
-         * 
-         * SE NÃO - Apenas grava o novo valor no array e bloqueia este item nos demais selects
-         * SE SIM - Libera este item nos demais Selects; 
-         *          Grava o novo valor no Array;
-         *          Bloqueia este novo valor de item nos demais Selects
-         */
-        let selected = 0; //PARA DETERMINAR QUAL SELECT FOI CLICACO.
-        document.querySelectorAll('select[name="fieldDestiny"]').forEach(select => {
-            debugger;
-            if(event.target!==select){
-                for(let i=0; i<select.options.length; i++){
-                    select.options[dbImport.ArrCombos[selected]].disabled=false;
-                }
-                select.options[event.target.options.selectedIndex].disabled=true;
-                dbImport.ArrCombos[selected] = event.target.options.selectedIndex;//???
-            }
-            selected++;
-        });
     }
 };
 dbImport.init();
--- view/scripts/dbImportModal.js
+++ view/scripts/dbImportModal.js
@@ -1,5 +1,145 @@
-let dbImport = {
+let dbImportModal = {
     init: () => {
+        
+        document.querySelector('#dbImportModalDriver').addEventListener('change', dbImportModal.setDriverName, false);        
+        document.querySelector('#dbImportModalHost').addEventListener('change', dbImportModal.setHost, false);
+        document.querySelector('#dbImportModalPort').addEventListener('change', dbImportModal.setPort, false);
+        document.querySelector('#dbImportModalDBName').addEventListener('change', dbImportModal.setDatabaseName, false);
+        document.querySelector('#btDbImportModalTest').addEventListener('click', dbImportModal.connTest, false);
 
+        dbImportModal.loadTable();
+        document.querySelector('#tableDbImportModal > tbody').addEventListener('click', dbImportModal.edit ,false);
+    },
+    loadTable: () => {
+        let url = lzInicial.host.concat('/dbimportconnections/pageable');
+        url += '?page='.concat(dbImportModal.page);
+        url += '&size='.concat(document.querySelector('#sizeDbImportModal').value);
+
+        fetch(url, { 
+            method: 'GET',
+            headers: lzInicial.headers
+        }).then(response => {
+                return response.json();            
+        }).then(response => {
+            return lzDataTable.list = response;
+        }).then(data => {
+            let options = {
+                data: data, 
+                page: dbImportModal.page, 
+                tabId: '#tableDbImportModal', 
+                pageId: '#paginationDbImportModal'
+            };
+            lzDataTable.setTable(options);
+        });
+    },
+    edit: (event) => {
+        event.preventDefault();
+        dbImportModal.tr = event.target.closest('tr');
+        let el = event.target.classList.contains('fa-pencil-square-o');
+        let chk = '';
+        if(el){
+            if(!lzInicial.hasAccess('dbImportModal', 'W')) {
+                //alertify.error('Você não possui acesso a esta função!');
+                //return false;
+            }
+            if(isDate(dbImportModal.tr.children[2].textContent, 'dd/MM/yyyy')){
+                chk = 'DISABLED';
+            }
+
+            let url = lzInicial.host.concat('/dbimportconnections/').concat(dbImportModal.tr.children[0].textContent);
+            fetch(url, { 
+                method: 'GET',
+                headers: lzInicial.headers
+            }).then(response => {
+                    return response.json();            
+            }).then(response => {
+                return lzDataTable.list = response;
+            }).then(data => {
+                dbImportModal.item = {
+                    dbImportModalId: data.id,
+                    dbImportModalConnName: data.connection,
+                    dbImportModalDriver: data.driver,
+                    dbImportModalHost: data.host,
+                    dbImportModalPort: data.port,
+                    dbImportModalDBName: data.dbName,
+                    dbImportModalOwner: data.owner,
+                    dbImportModalUserName: data.userName,
+                    dbImportModalPassword: data.password,
+                    dbImportModalURL: data.url,
+                    dbImportModalDisabled: chk
+                };
+                lzInicial.populate(document.querySelector('#formDbImportModal'), dbImportModal.item);
+            });
+        }
+    },
+    item: {},
+    tr: null,
+    page: 0,
+    list: [],
+    driver: "",
+    hostname: "",
+    port: "",
+    databaseName: "",
+    setDriverName: (event) => {
+
+        /** Java JDBC Driver for Microsoft Access Database
+            <dependency>
+                <groupId>net.sf.ucanaccess</groupId>
+                <artifactId>ucanaccess</artifactId>
+                <version>4.0.4</version>
+            </dependency>
+         */
+        
+        let driverName = document.querySelector('#dbImportModalDriver');        
+
+        switch (driverName[driverName.selectedIndex].value) {
+            case 'ORACLE':
+                dbImportModal.driver = 'jdbc:oracle:thin:';
+                break;
+            case 'MYSQL':
+                dbImportModal.driver = 'jdbc:mysql:';
+                break;
+            case 'POSTGRE':
+                dbImportModal.driver = 'jdbc:postgresql:';
+                break;
+            case 'SQL_SERVER':
+                dbImportModal.driver = 'jdbc:ucanaccess:';
+                break;
+            case 'ACCESS':
+                dbImportModal.driver = 'jdbc:ucanaccess:';
+                break;
+        }
+        dbImportModal.setURL();        
+    },
+    setHost: () => {
+        let separador = '//';
+        if(dbImportModal.driver.includes('oracle')) separador = '@';
+        dbImportModal.hostname = separador + event.target.value;
+        dbImportModal.setURL(); 
+    },
+    setPort: () => {
+        let separador = ':';
+        dbImportModal.port = separador + event.target.value;
+        dbImportModal.setURL(); 
+    },
+    setDatabaseName: () => {
+        let separador = '/';
+        dbImportModal.databaseName = separador + event.target.value;
+        dbImportModal.setURL(); 
+    },
+    setURL: () => {
+        let url = document.querySelector('#dbImportModalURL');
+        url.value = dbImportModal.driver + dbImportModal.hostname + dbImportModal.port + dbImportModal.databaseName;
+    },
+    connTest: () => {
+
+        let driverName = document.querySelector('#dbImportModalDriver');
+
+        if(driverName[driverName.selectedIndex].value.includes('ORACLE')){
+            alertify.success('Connection successfully tested!'); 
+        }else{
+            alertify.error('Cannot open connection!');
+        }
     }
 };
+dbImportModal.init();
